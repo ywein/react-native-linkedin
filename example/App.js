c1012d28529409f6fc8b332873ab29330649b8e2
Xavier Carpentier <xcapetir@gmail.com>
2017-12-22 22:47:41 +0100

    example

--- example/App.js
+++ example/App.js
@@ -7,7 +7,12 @@ import {
   Text,
   Clipboard,
   Button,
+  Image,
+  ActivityIndicator,
+  StatusBar,
 } from 'react-native'
+
+import { CLIENT_ID, CLIENT_SECRET } from 'react-native-dotenv'
 
 import LinkedInModal from 'react-native-linkedin'
 
@@ -18,38 +23,124 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  userContainer: {
+    padding: 10,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  picture: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    resizeMode: 'cover',
+    marginBottom: 15,
+  },
+  item: {
+    flexDirection: 'row',
+    marginVertical: 5,
+  },
+  label: {
+    marginRight: 10,
+  },
+  value: {
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+  linkedInContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  labelContainer: {
+    flex: 0.7,
+    alignItems: 'flex-end',
+  },
+  valueContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
 })
 
 export default class AppContainer extends React.Component {
   state = {
     access_token: undefined,
     expires_in: undefined,
+    refreshing: false,
   }
+
+  constructor(props) {
+    super(props)
+    StatusBar.setHidden(true)
+  }
+
+  async getUser({ access_token }) {
+    this.setState({ refreshing: true })
+    const baseApi = 'https://api.linkedin.com/v1/people/'
+    const qs = { format: 'json' }
+    const params = [
+      'first-name',
+      'last-name',
+      'picture-urls::(original)',
+      'headline',
+      'email-address',
+    ]
+
+    const response = await fetch(`${baseApi}~:(${params.join(',')})?format=json`, {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + access_token,
+      },
+    })
+    const payload = await response.json()
+    this.setState({ ...payload, refreshing: false })
+  }
+
+  renderItem(label, value) {
+    return (
+      <View style={styles.item}>
+        <View style={styles.labelContainer}>
+          <Text style={styles.label}>{label}</Text>
+        </View>
+        <Text>👉</Text>
+        <View style={styles.valueContainer}>
+          <Text style={styles.value}>{value}</Text>
+        </View>
+      </View>
+    )
+  }
+
   render() {
-    const { expires_in, access_token } = this.state
+    const { emailAddress, pictureUrls, refreshing, firstName, lastName, headline } = this.state
     return (
       <View style={styles.container}>
-        <LinkedInModal
-          ref={ref => {
-            this.modal = ref
-          }}
-          clientID="86vrfyx76mucrq"
-          clientSecret="as8w6lkXydkY94Is"
-          redirectUri="https://xaviercarpentier.com"
-          onSuccess={this.setState.bind(this)}
-        />
-        {access_token && (
-          <Text onPress={() => Clipboard.setString(access_token)}>
-            {'\n'}access_token: {access_token} {'\n'}
-          </Text>
+        {!emailAddress &&
+          !refreshing && (
+            <View style={styles.linkedInContainer}>
+              <LinkedInModal
+                ref={ref => {
+                  this.modal = ref
+                }}
+                clientID={CLIENT_ID}
+                clientSecret={CLIENT_SECRET}
+                redirectUri="https://xaviercarpentier.com"
+                onSuccess={data => this.getUser(data)}
+              />
+              <Button title="Open from external" onPress={() => this.modal.open()} />
+            </View>
+          )}
+
+        {refreshing && <ActivityIndicator size="large" />}
+
+        {emailAddress && (
+          <View style={styles.userContainer}>
+            <Image style={styles.picture} source={{ uri: pictureUrls.values[0] }} />
+            {this.renderItem('Email', emailAddress)}
+            {this.renderItem('First name', firstName)}
+            {this.renderItem('Last name', lastName)}
+            {this.renderItem('Headline', headline)}
+          </View>
         )}
-        {expires_in && (
-          <Text onPress={() => Clipboard.setString(`${expires_in}`)}>
-            expires_in: {expires_in}
-            {'\n'}
-          </Text>
-        )}
-        <Button title="Open from external" onPress={() => this.modal.open()} />
       </View>
     )
   }
